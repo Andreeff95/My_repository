e57a7d761ceef3fc402066f0e7ce1019ac65657f
andreeff95 <i.a.v.95@mail.ru>
2023-04-18 02:48:52 +0400

    Перевел все папки на анг, добавил гит игнор

---
+++ Sem1_Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число: ");
+string num_a = Console.ReadLine();
+Console.Write("Введите второе число: ");
+string num_b = Console.ReadLine();
+if(int.Parse(num_a)>int.Parse(num_b))
+{
+    Console.Write("max = ");
+    Console.WriteLine(num_a);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(num_b);
+}
---
+++ Sem1_Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите первое число: ");
+string num_a = Console.ReadLine();
+Console.Write("Введите второе число: ");
+string num_b = Console.ReadLine();
+Console.Write("Введите третье число: ");
+string num_c = Console.ReadLine();
+int a = int.Parse(num_a);
+int b = int.Parse(num_b);
+int c = int.Parse(num_c);
+int max = a;
+
+if(b>max)
+{
+    max = b;
+}
+if(c>max)
+{
+    max = c;
+}
+Console.Write("max = ");
+Console.WriteLine(max);
