c9fa198a688507f1d12cd9a792e323ea0d731263
andreeff95 <i.a.v.95@mail.ru>
2023-04-18 02:51:25 +0400

    Привел все файлы первого семинара в порядок, готовлюсь к пушу в свой репозиторий на гихабе

---
+++ Sem1_Task6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+string num_a = Console.ReadLine();
+if(int.Parse(num_a)%2==0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
---
+++ Sem1_Task8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число N: ");
+string N = Console.ReadLine();
+int i = 0;
+while(i<=int.Parse(N))
+{
+    if(i%2==0)
+    {
+        Console.Write(i + " ");
+    }
+    i++;
+}
