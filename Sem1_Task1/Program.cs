8bcd8083ba3981ed66de78a39e545e8ea73846d1
andreeff95 <i.a.v.95@mail.ru>
2023-04-18 21:23:28 +0400

    Решена первая задача первого семинара

---
+++ Sem1_Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Python:
+num_1 = int(input())
+num_2 = int(input())
+if num_1 = num_2**2:
+    print("Да")
+else:
+    print("Нет")
+*/
+
+Console.Write("Введите первое число: ");
+string num_1 = Console.ReadLine();
+Console.Write("Введите второе число: ");
+string num_2 = Console.ReadLine();
+if(Convert.ToInt32(num_1)==(Convert.ToInt32(num_2))*(Convert.ToInt32(num_2)))
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
