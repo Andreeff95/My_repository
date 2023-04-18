d4b4b2150579fb1138e0c1f45db7a0a8de8806f0
andreeff95 <i.a.v.95@mail.ru>
2023-04-18 22:11:37 +0400

    Решил задачи 3 и 5 из первого семинара

---
+++ Sem1_Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿System.Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+switch(N){
+    case 1:{
+        System.Console.WriteLine("Понедельник");
+    }
+    case 2:{
+        System.Console.WriteLine("Вторник");
+    }
+    case 3:{
+        System.Console.WriteLine("Среда");
+    }
+    case 4:{
+        System.Console.WriteLine("Четверг");
+    }
+    case 5:{
+        System.Console.WriteLine("Пятница");
+    }
+    case 6:{
+        System.Console.WriteLine("Суббота");
+    }
+    case 7:{
+        System.Console.WriteLine("Воскресенье");
+    }
+    default:{
+        System.Console.WriteLine("Число несоответствует дню недели");
+    }
+}
---
+++ Sem1_Task5/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Напишите программу, которая на вход принимает одно число N, а на выходе показывает все целые числа в промежутке от -N до N. 
+N = int(input())
+N_neg = N-(2*N)
+for i in range(N_neg, N+1):
+    print(i)
+*/
+System.Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+for(int i=(N-(2*N)); i<N; i++){
+    System.Console.Write(i+", ");
+}
+System.Console.WriteLine(N);
