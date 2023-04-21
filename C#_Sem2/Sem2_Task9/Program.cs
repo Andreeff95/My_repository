f9fb7d9751698819e84e274a621f50e93dac2534
andreeff95 <i.a.v.95@mail.ru>
2023-04-21 12:59:23 +0400

    Исправлено решение 9 задачи

--- C#_Sem2/Sem2_Task9/Program.cs
+++ C#_Sem2/Sem2_Task9/Program.cs
@@ -8,14 +8,10 @@ if len(N)==2:
 else:
     print("Введено неверное число")
 */
-System.Console.Write("Введите число от 10 до 99: ");
-int N = System.Convert.ToInt32(System.Console.ReadLine());
-if(N>=10 && N<=99){
-    if(N/10>N%10){
-        System.Console.WriteLine(N/10);
-    } else {
-        System.Console.WriteLine(N%10);
-    }
+
+int N = new Random().Next(10, 100);
+if(N/10>N%10){
+    System.Console.WriteLine(N/10);
 } else {
-    System.Console.WriteLine("Введено неверное число");
+    System.Console.WriteLine(N%10);
 }
