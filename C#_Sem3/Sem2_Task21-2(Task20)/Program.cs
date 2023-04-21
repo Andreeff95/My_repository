a1cfcbda480aadce117de06f09fa66a535500390
andreeff95 <i.a.v.95@mail.ru>
2023-04-21 20:48:47 +0400

    Подправлено решение задачи 21-2

--- C#_Sem3/Sem2_Task21-2(Task20)/Program.cs
+++ C#_Sem3/Sem2_Task21-2(Task20)/Program.cs
@@ -3,34 +3,34 @@
 ними в 3D пространстве.
 */ 
 
-double GetDist(int side1, int side2)
+double GetDist(double side1, double side2)
 {
     double dist = Math.Sqrt(Math.Pow(side1, 2) + Math.Pow(side2, 2));
     return dist;
 }
 
-int GetSide(int num1, int num2)
+double GetSide(double num1, double num2)
 {
-    int side = Math.Abs(num1 - num2);
+    double side = Math.Abs(num1 - num2);
     return side;
 }
 
 Console.Write("Введите координату X для первой точки: ");
-int x1 = Convert.ToInt32(Console.ReadLine());
+double x1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите координату Y для первой точки: ");
-int y1 = Convert.ToInt32(Console.ReadLine());
+double y1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите координату Z для третьей точки: ");
-int z1 = Convert.ToInt32(Console.ReadLine());
+double z1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите координату X для второй точки: ");
-int x2 = Convert.ToInt32(Console.ReadLine());
+double x2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите координату Y для второй точки: ");
-int y2 = Convert.ToInt32(Console.ReadLine());
+double y2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите координату Z для третьей точки: ");
-int z2 = Convert.ToInt32(Console.ReadLine());
+double z2 = Convert.ToInt32(Console.ReadLine());
 
-int side_x = GetSide(x1, x2);
-int side_y = GetSide(y1, y2);
-int side_z = GetSide(z1, z2);
+double side_x = GetSide(x1, x2);
+double side_y = GetSide(y1, y2);
+double side_z = GetSide(z1, z2);
 double temp_res = GetDist(side_x, side_y);
-double get_res = Math.Round(GetDist(side_z, Convert.ToInt32(temp_res)), 2);
+double get_res = Math.Round(GetDist(side_z, temp_res), 2);
 System.Console.WriteLine($"Расстояние от первой точки до второй равно: {get_res}");
