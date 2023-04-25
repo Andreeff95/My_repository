5427552d33b0507a3a7071c7d62005901e5e17fa
andreeff95 <i.a.v.95@mail.ru>
2023-04-25 13:32:39 +0400

    ПОдправлено решение задачи 37

--- C#_Sem5/Sem5_Task37/Program.cs
+++ C#_Sem5/Sem5_Task37/Program.cs
@@ -3,9 +3,10 @@
 и т.д. Результат запишите в новом массиве.
 */ 
 
-int[] my_array = new int[4];
-int[] new_array = new int[my_array.Length/2];
 Random rnd = new Random();
+int[] my_array = new int[rnd.Next(4, 8)];
+int[] new_array = new int[my_array.Length/2];
+
 void RandArray(int[] arr)
 {
     for(int i=0; i<arr.Length; i++)
@@ -27,15 +28,21 @@ string PrintArray(int[] arr)
     return res;
 }
 
-void FillArray(int[] arr1, int[] arr2)
+string FillArray(int[] arr1, int[] arr2)
 {
     for(int i=0; i<arr2.Length; i++)
     {
         arr2[i] = arr1[i]*arr1[arr1.Length-(i+1)];
+    }
+    if(arr1.Length%2==1)
+    {
+        return $", {Convert.ToString(arr1[arr1.Length/2])}";
+    } else {
+        return "";
     }
 }
 
 RandArray(my_array);
 System.Console.Write($"[{PrintArray(my_array)}] -> ");
 FillArray(my_array, new_array);
-System.Console.WriteLine($"{PrintArray(new_array)}");
+System.Console.WriteLine($"{PrintArray(new_array)}{FillArray(my_array, new_array)}");
