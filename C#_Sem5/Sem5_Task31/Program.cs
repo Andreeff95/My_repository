a49a04fb11161c9c30b7cb1fb4adf083ac0ba166
andreeff95 <i.a.v.95@mail.ru>
2023-04-25 12:42:41 +0400

    Задачи 5 семинара-1

--- C#_Sem5/Sem5_Task31/Program.cs
+++ C#_Sem5/Sem5_Task31/Program.cs
@@ -3,62 +3,65 @@
 отрицательных и положительных элементов массива.
 */ 
 
-int[] my_array = new int[12];
-Random rnd = new Random();
-void RandArray(int[] arr)
+void Task31()
 {
-    for(int i=0; i<arr.Length; i++)
+    int[] my_array = new int[12];
+    Random rnd = new Random();
+    void RandArray(int[] arr)
     {
-        int add = rnd.Next(-9, 10);
-        arr[i] = add;
-    }
-}
-
-void PrintArray(int[] arr)
-{
-    foreach(var v in arr)
-    {
-        System.Console.Write($"{v}, ");
-    }
-    System.Console.WriteLine("");
-}
-
-int SumPlus(int[] arr)
-{
-    int plus = 0;
-    for(int i=0; i<arr.Length; i++)
-    {
-        if (arr[i] >= 0)
+        for(int i=0; i<arr.Length; i++)
         {
-            plus += arr[i];
-        }
-        else
-        {
-            continue;
-        }
-    }
-    return plus; 
-}
-
-int SumMinus(int[] arr)
-{
-    int minus = 0;
-    for(int i=0; i<arr.Length; i++)
-    {
-        if (arr[i] >= 0)
-        {
-            continue;
-        }
-        else
-        {
-            minus += arr[i];
+            int add = rnd.Next(-9, 10);
+            arr[i] = add;
         }
     }
 
-    return minus; 
+    void PrintArray(int[] arr)
+    {
+        foreach(var v in arr)
+        {
+            System.Console.Write($"{v}, ");
+        }
+        System.Console.WriteLine("");
+    }
+
+    int SumPlus(int[] arr)
+    {
+        int plus = 0;
+        for(int i=0; i<arr.Length; i++)
+        {
+            if (arr[i] >= 0)
+            {
+                plus += arr[i];
+            }
+            else
+            {
+                continue;
+            }
+        }
+        return plus; 
+    }
+
+    int SumMinus(int[] arr)
+    {
+        int minus = 0;
+        for(int i=0; i<arr.Length; i++)
+        {
+            if (arr[i] >= 0)
+            {
+                continue;
+            }
+            else
+            {
+                minus += arr[i];
+            }
+        }
+
+        return minus; 
+    }
+
+    RandArray(my_array);
+    PrintArray(my_array);
+    System.Console.WriteLine($"Сумма положительных чисел: {SumPlus(my_array)}. Сумма отрицательных чисел: {SumMinus(my_array)}.");
 }
-
-RandArray(my_array);
-PrintArray(my_array);
-System.Console.WriteLine($"Сумма положительных чисел: {SumPlus(my_array)}. Сумма отрицательных чисел: {SumMinus(my_array)}.");
-
+Task31();
